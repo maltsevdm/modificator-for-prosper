a3663f07e9c5c0c4dffbdfc97069f1d7b865f21f
Maltsev.DM <dimamalcev777@gmail.com>
2023-12-19 11:27:44 +0500

    Initial Commit

---
+++ file_settings.py
@@ -0,0 +1,17 @@
+COLS_WOP = {
+    'date': 1, 'status': 2, 'dynamic_level': 3, 'pres_buf': 4, 'pres_zatr': 5,
+    'pres_lin': 6, 'pres_downhole': 7, 'diametr_fit_zatr': 8,
+    'diametr_fit_buf': 9, 'Q_liq': 10, 'WC': 11, 'Q_oil': 12, 'Q_gaz': 13,
+    'WC_BCH': 14, 'GF': 15, 'temp_liq': 16, 'KVCH': 17, 'oil_density': 18,
+    'water_density': 19, 'pump': 20, 'amper': 21, 'load': 22, 'frequency': 23,
+    'Ppr': 24, 'Tpr': 25, 'downtime': 26, 'mrp': 27, 'note': 28,
+    'prosper_Q_liq': 29, 'prosper_Q_liq_delta': 30, 'corr': 31, 'coef_corr': 32,
+    'pump_wear': 33
+}
+COLS_PR_SET = {
+    'inp_data': {'date': 2, 'Q_liq_moment': 3, 'Q_liq': 4, 'pres_buf': 5,
+                 'Q_gaz': 6, 'GF': 7, 'WC': 8, 'frequency': 9, 'downtime': 10,
+                 'Ppr': 11},
+    'vlp_ipr': {'Ppr': 12, 'd_Ppr': 13},
+    'sys': {'Q_liq': 14, 'd_Q_liq': 15, 'Ppr': 16}
+}
---
+++ fonts.py
@@ -0,0 +1,3 @@
+FONT_BUTTONS = 'Calibri 9 bold'
+FONT_LABELS = 'Calibri 11 bold'
+FONT_TEXT = 'Calibri 11'
